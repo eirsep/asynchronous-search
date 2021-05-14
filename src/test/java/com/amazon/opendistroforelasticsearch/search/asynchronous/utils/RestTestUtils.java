198d51b6062fe459fcaa3bab3ecd3a0d104b99d9
Surya Nistala <snistala@amazon.com>
2021-05-14 20:06:11 +0530

    randomize usage of legacy and current base uri in rest tests

--- src/test/java/com/amazon/opendistroforelasticsearch/search/asynchronous/utils/RestTestUtils.java
+++ src/test/java/com/amazon/opendistroforelasticsearch/search/asynchronous/utils/RestTestUtils.java
@@ -42,6 +42,7 @@ import org.opensearch.action.support.IndicesOptions;
 import org.opensearch.client.Request;
 import org.opensearch.cluster.health.ClusterHealthStatus;
 import org.opensearch.common.Priority;
+import org.opensearch.common.Randomness;
 import org.opensearch.common.Strings;
 import org.opensearch.common.SuppressForbidden;
 import org.opensearch.common.lucene.uid.Versions;
@@ -72,11 +73,11 @@ public class RestTestUtils {
     public static Request buildHttpRequest(SubmitAsynchronousSearchRequest submitAsynchronousSearchRequest) throws IOException {
 
         SearchRequest searchRequest = submitAsynchronousSearchRequest.getSearchRequest();
-        String baseUri = new Random().nextBoolean() ? AsynchronousSearchPlugin.BASE_URI
+        String baseUri = Randomness.get().nextBoolean() ? AsynchronousSearchPlugin.BASE_URI
                 : AsynchronousSearchPlugin.LEGACY_BASE_URI;
         Request request = new Request(HttpPost.METHOD_NAME,
                 /*trim first backslash*/
-                endpoint(searchRequest.indices(), AsynchronousSearchPlugin.BASE_URI.substring(1)));
+                endpoint(searchRequest.indices(), baseUri.substring(1)));
 
         Params params = new Params();
         addSearchRequestParams(params, searchRequest);
