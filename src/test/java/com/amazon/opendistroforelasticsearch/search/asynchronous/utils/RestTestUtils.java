c8dfe54925eecfa3f6b6c74a2016edb4f21490c3
Surya Nistala <snistala@amazon.com>
2021-05-14 20:07:51 +0530

    randomize usage of legacy and current base uri in rest tests

--- src/test/java/com/amazon/opendistroforelasticsearch/search/asynchronous/utils/RestTestUtils.java
+++ src/test/java/com/amazon/opendistroforelasticsearch/search/asynchronous/utils/RestTestUtils.java
@@ -64,7 +64,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Random;
 import java.util.StringJoiner;
 
 public class RestTestUtils {
