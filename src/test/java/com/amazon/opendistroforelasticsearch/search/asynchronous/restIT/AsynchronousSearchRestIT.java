7a1632aabbda92057fb6b721bef48827687705fb
Surya Nistala <snistala@amazon.com>
2021-05-19 17:28:25 +0530

    test get and delete bwc api

--- src/test/java/com/amazon/opendistroforelasticsearch/search/asynchronous/restIT/AsynchronousSearchRestIT.java
+++ src/test/java/com/amazon/opendistroforelasticsearch/search/asynchronous/restIT/AsynchronousSearchRestIT.java
@@ -31,6 +31,7 @@ import com.amazon.opendistroforelasticsearch.search.asynchronous.request.DeleteA
 import com.amazon.opendistroforelasticsearch.search.asynchronous.request.GetAsynchronousSearchRequest;
 import com.amazon.opendistroforelasticsearch.search.asynchronous.request.SubmitAsynchronousSearchRequest;
 import com.amazon.opendistroforelasticsearch.search.asynchronous.response.AsynchronousSearchResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.opensearch.action.search.SearchRequest;
@@ -161,7 +162,17 @@ public class AsynchronousSearchRestIT extends AsynchronousSearchRestTestCase {
     public void testBackwardCompatibilityWithOpenDistro() throws IOException {
         Request request = new Request(HttpPost.METHOD_NAME, AsynchronousSearchPlugin.LEGACY_OPENDISTRO_BASE_URI +
                 "?keep_on_completion=true");
+
         Response resp = client().performRequest(request);
+        assertEquals(resp.getStatusLine().getStatusCode(), 200);
+        AsynchronousSearchResponse asynchronousSearchResponse = parseEntity(resp.getEntity(), AsynchronousSearchResponse::fromXContent);
+        request = new Request(HttpGet.METHOD_NAME,
+                AsynchronousSearchPlugin.LEGACY_OPENDISTRO_BASE_URI + "/" + asynchronousSearchResponse.getId());
+        resp = client().performRequest(request);
+        assertEquals(resp.getStatusLine().getStatusCode(), 200);
+        request = new Request(HttpDelete.METHOD_NAME,
+                AsynchronousSearchPlugin.LEGACY_OPENDISTRO_BASE_URI + "/" + asynchronousSearchResponse.getId());
+        resp = client().performRequest(request);
         assertEquals(resp.getStatusLine().getStatusCode(), 200);
         request = new Request(HttpGet.METHOD_NAME, AsynchronousSearchPlugin.LEGACY_OPENDISTRO_BASE_URI + "/stats");
         resp = client().performRequest(request);
